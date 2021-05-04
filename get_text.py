ca73d06d9952dec8705911b65af4e22e9405405d
Nathan Springman <naspringman@gmail.com>
2021-05-04 00:07:06 -0500

    pass in feeling number

--- get_text.py
+++ get_text.py
@@ -13,15 +13,13 @@ texts = [
     """I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces."""
 ]
 
-def generate(text):
+def generate(feeling):
     generatedText = requests.post(
         "https://api.deepai.org/api/text-generator",
         data={
-            'text': text,
+            'text': texts[feeling-1],
         },
         headers={'api-key': 'cba42b1d-0cf8-40f1-b37f-6615ac018163'}
     )
     generatedText = generatedText.json()['output'] + '.'
     return generatedText
-
-print(generate(texts[8]))
