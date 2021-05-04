40fb73ed61dc8c1641a421576243d4286545e09a
Nathan Springman <naspringman@gmail.com>
2021-05-03 23:53:34 -0500

    add texts back

--- get_text.py
+++ get_text.py
@@ -1,5 +1,11 @@
 import requests
 
+texts = [
+    'They are to carry the tabernacle and all its furnishings; they are to take care of it and encamp around it.',
+    'The Congress shall have Power To lay and collect Taxes, Duties, Imposts and Excises, to pay the Debts and provide for the common Defence and general Welfare of the United States; but all Duties, Imposts and Excises shall be uniform throughout the United States;',
+    'Oh, maternal ditch, half full of muddy water! A factory gutter! As I raised my body, mud-spattered and smelly, I felt the red hot poker of joy deliciously pierce my heart. A crowd of fishermen and gouty naturalists crowded terrified around this marvel. '
+]
+
 def generate(text):
     generatedText = requests.post(
         "https://api.deepai.org/api/text-generator",
@@ -10,3 +16,5 @@ def generate(text):
     )
     generatedText = generatedText.json()['output'] + '.'
     return generatedText
+
+print(generate(texts[2]))
